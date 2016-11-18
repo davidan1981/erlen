f416e8249bc73b2f48ae6caeeb8b6988833abd75
David An <davidan1981@gmail.com>
2016-11-17 20:40:04 -0600

    Rename ResourceListSchema to ResourceArrayOf
    - it's really not a schema but a schema generator

--- lib/erlen/basic_schemas/resource_array_of.rb
+++ lib/erlen/basic_schemas/resource_array_of.rb
@@ -1,10 +1,10 @@
 module Erlen
   # This class dynamically generates a concrete schema class that represents
   # a collection type for resources. It adheres to Hireology's API standard.
-  class ResourceListSchema
-    def self.new(resourceSchema)
+  class ResourceArrayOf
+    def self.new(resource_schema)
       Class.new(Schema::Base) do
-        attribute :data, Schema::ArrayOf.new(resourceSchema)
+        attribute :data, Schema::ArrayOf.new(resource_schema), required: true
         attribute :page, Integer
         attribute :page_size, Integer
         attribute :count, Integer
