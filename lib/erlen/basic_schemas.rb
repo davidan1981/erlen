cb9c8bc38a3da3ed2c34d33da1629109b1328b1e
David An <davidan1981@gmail.com>
2016-11-30 13:04:51 -0600

    Rename pre-defined schema files

--- lib/erlen/basic_schemas.rb
+++
@@ -1,4 +0,0 @@
-require_relative 'basic_schemas/empty_schema'
-require_relative 'basic_schemas/any_schema'
-require_relative 'basic_schemas/resource_schema'
-require_relative 'basic_schemas/resource_list_schema'
